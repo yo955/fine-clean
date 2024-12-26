e0cc489a4e75911246b8f445260c0458ccfd6254
Joo <yo955@gmail.com>
2024-12-26 07:40:07 -0800

    edit logo

--- src/app/_components/common/header/hero/Logo.tsx
+++ src/app/_components/common/header/hero/Logo.tsx
@@ -10,7 +10,7 @@ export const Logo: React.FC<LogoProps> = ({ className, src }) => {
   return (
     <div className="relative">
       <div className={`absolute ${className}`}>
-        <Image src={src} alt="logo" width={200} height={200} className="w-[90%] h-[90px] md:h-full md:w-[80%]" />
+        <Image src={src} alt="logo" width={200} height={200} className="w-[90%] h-[90px] md:h-auto md:w-[80%]" />
       </div>
     </div>
   );
