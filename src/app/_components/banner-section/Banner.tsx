2c761162e19589f58baa7fade60b061733559c9e
Joo <yo955@gmail.com>
2024-12-27 01:28:17 -0800

    finsish banner section and responsive

--- src/app/_components/banner-section/Banner.tsx
+++ src/app/_components/banner-section/Banner.tsx
@@ -14,43 +14,41 @@ const audioFilesRight = [
 const Banner = () => {
   return (
     <section className="banner-section">
-      <div className="overlay">
-        <div className="container flex flex-col justify-center items-center">
-          <div className="content  flex flex-col gap-2">
-            <div className="title">
-              <h2 className="text-4xl text-orange">
-                شركة آمگــو للتنظيف ونقل الأثاث بالرياض
-              </h2>
-            </div>
-            <div className="comment text-center">
-              <h2 className="text-3xl">تعليقات صوتية لعملائنا</h2>
-            </div>
+      <div className="container w-full flex flex-col justify-center items-center">
+        <div className="content m-5 flex flex-col gap-2">
+          <div className="title">
+            <h2 className="text-3xl lg:text-4xl text-orange text-center">
+              شركة آمگــو للتنظيف ونقل الأثاث بالرياض
+            </h2>
           </div>
-          <div className="recorder-comments flex flex-col md:flex-row gap-5">
-            <div className="left">
-              <ul className="flex flex-col gap-3">
-                {audioFilesLeft.map((file, index) => (
-                  <li key={index}>
-                    <audio controls>
-                      <source src={file.path} type="audio/wav" /> متصفحك لا يدعم
-                      تشغيل الملفات الصوتية.
-                    </audio>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="right">
-              <ul className="flex flex-col gap-3">
-                {audioFilesRight.map((file, index) => (
-                  <li key={index}>
-                    <audio controls>
-                      <source src={file.path} type="audio/wav" /> متصفحك لا يدعم
-                      تشغيل الملفات الصوتية.
-                    </audio>
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="comment text-center">
+            <h2 className="text-3xl text-white">تعليقات صوتية لعملائنا</h2>
+          </div>
+        </div>
+        <div className="recorder-comments flex flex-col md:flex-row gap-5">
+          <div className="left">
+            <ul className="flex flex-col gap-3 ">
+              {audioFilesLeft.map((file, index) => (
+                <li key={index}>
+                  <audio controls>
+                    <source src={file.path} type="audio/wav" /> متصفحك لا يدعم
+                    تشغيل الملفات الصوتية.
+                  </audio>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="right">
+            <ul className="flex flex-col gap-3">
+              {audioFilesRight.map((file, index) => (
+                <li key={index}>
+                  <audio controls>
+                    <source src={file.path} type="audio/wav" /> متصفحك لا يدعم
+                    تشغيل الملفات الصوتية.
+                  </audio>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
