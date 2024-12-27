69f7be8df1c7177a80f40a47260bb9dd509bc79c
Joo <yo955@gmail.com>
2024-12-27 00:46:07 -0800

    edit image

--- src/app/_components/hero-section/LeftSection.tsx
+++ src/app/_components/hero-section/LeftSection.tsx
@@ -6,16 +6,16 @@ const LeftSection = () => {
     <div className="flex flex-wrap justify-center">
       <div className="p-5 text-center w-full flex justify-center items-center">
         <h3 className="text-[1.2rem] md:text-[1.5rem] lg:text-[1.75rem] p-1 font-mono font-black text-[#333]">
-        اهلاً وسهلاً بكم في آمكو
+          اهلاً وسهلاً بكم في آمكو
         </h3>
       </div>
-      <div className="relative w-full min-h-full">
+      <div className="w-full min-h-full">
         <Image
           src={"/images/saudi-man.jpg"}
           alt="hero-image"
           width={1000}
           height={1000}
-          className="absolute bg-cover"
+          className="bg-cover"
           priority
         />
       </div>
