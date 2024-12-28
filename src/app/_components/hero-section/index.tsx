ebb085c052b0b26f8399516b29a684d5c3ddb842
Joo <yo955@gmail.com>
2024-12-27 22:21:19 -0800

    add float icons

--- src/app/_components/hero-section/index.tsx
+++ src/app/_components/hero-section/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LeftSection from "./LeftSection";
 import RightSections from "./RightSections";
+import FloatingIcons from "../common/FloatingIcons";
 
 const HeroSection = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 mb-10">
       <RightSections />
       <LeftSection />
+      <FloatingIcons />
     </section>
   );
 };
