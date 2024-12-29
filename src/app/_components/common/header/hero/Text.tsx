3ffd2f21ba77582b4972efe70bcf110a0dcbd98f
Joo <yo955@gmail.com>
2024-12-29 05:38:54 -0800

    edit fon size

--- src/app/_components/common/header/hero/Text.tsx
+++ src/app/_components/common/header/hero/Text.tsx
@@ -8,12 +8,12 @@ export const Text: React.FC<TextProps> = ({ className }) => {
     <div className={`${className}`}>
       <div className="text flex justify-around items-center h-full">
         <div className="arabic max-h-fit ">
-          <h1 className="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary font-bold tracking-wide lg:tracking-[0.08rem]">
+          <h1 className="text-[2rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary font-bold tracking-wide lg:tracking-[0.08rem]">
           آمـ<span className="text-white">گــ</span>و
           </h1>
         </div>
         <div className="english">
-          <h1 className="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary tracking-[0.2rem] md:tracking-[0.5rem] lg:tracking-[0.7rem]">
+          <h1 className="text-[2rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary tracking-[0.2rem] md:tracking-[0.5rem] lg:tracking-[0.7rem]">
             AM<span className="text-white">C</span>O
           </h1>
         </div>
