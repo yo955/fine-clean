ac94675a935f70be7a132e780c231e5bd37ee4a2
joo <joo@gmail.com>
2025-01-07 18:54:16 +0200

    edit map location

--- src/app/_components/common/MapComponent.tsx
+++ src/app/_components/common/MapComponent.tsx
@@ -11,7 +11,7 @@ const MapComponent: React.FC<MapProps> = ({ width, radius, className }) => {
       <div className="w-full mx-auto relative">
         <div style={{ maxWidth: `${width}%` }} className="mx-auto">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37282.025245702156!2d46.72438098059433!3d24.796705476103956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2efcfe88f3ac8d%3A0x4aa75e1c02fadc82!2zUkFGQTI4OTPYjCAyODkzINin2YTYtNmK2K4g2LnYqNiv2KfZhNmE2Ycg2KfZhNmF2K7YttmI2KjYjCA3MTk22Iwg2K3ZiiDYp9mE2YHZhNin2K3YjCDYp9mE2LHZitin2LYgMTMzMTTYjCDYp9mE2LPYudmI2K_Zitip!5e0!3m2!1sar!2seg!4v1733359246876!5m2!1sar!2seg"
+            src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d57937.812658991395!2d46.776295!3d24.825799!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjTCsDQ5JzMyLjkiTiA0NsKwNDYnMzQuNyJF!5e0!3m2!1sen!2sus!4v1736268648584!5m2!1sen!2sus"
             style={{ borderRadius: `${radius}` }}
             className={className}
             width="100%"
