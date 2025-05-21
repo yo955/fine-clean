4ef362715c90cef9b9c730c178f3d67fc3aceb2a
youssef shaaban <142118334+yo955@users.noreply.github.com>
2025-05-21 06:20:38 -0700

    edit

--- src/app/_components/common/header/hero/Hero.tsx
+++ src/app/_components/common/header/hero/Hero.tsx
@@ -5,10 +5,10 @@ import { Text } from "./Text";
 export const Hero = () => {
   return (
     <main className="grid grid-cols-6 mt-12  ">
-      <Text className="bg-orange col-span-4 text-center p-4 " />
+      <Text className="bg-orange col-span-4 text-center p-2 md:p-4 " />
       <Logo
         src="/logo/logo.png"
-        className="flex justify-center items-center w-[130%] h-[300%] bg-cover"
+        className="flex justify-center items-center w-[130%] h-[200%] bg-cover"
       />
       <div className="bg-primary"></div>
     </main>
--- src/app/_components/common/header/hero/Text.tsx
+++ src/app/_components/common/header/hero/Text.tsx
@@ -7,18 +7,26 @@ interface TextProps {
 export const Text: React.FC<TextProps> = ({ className = "" }) => {
   return (
     <div className={`${className}`}>
-      <div className="flex flex-col items-center text-center h-full space-y-2">
+      <div className="flex flex-col items-center text-center h-full space-y-0">
         {/* الشعار: عربي + إنجليزي */}
         <div className="flex items-center gap-4">
           <h1 className="text-[1.7rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary font-bold tracking-wide lg:tracking-[0.08rem]">
             آمـ<span className="text-white">گــ</span>و
           </h1>
-          <h1 className="text-[1.7rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary tracking-[0.2rem] md:tracking-[0.5rem] lg:tracking-[0.7rem] font-semibold">
+          <h1 className="text-[1.7rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.7rem] text-primary tracking-[0.2rem] md:tracking-[0.5rem] lg:tracking-[0.2rem] font-semibold">
             AM<span className="text-white">C</span>O
           </h1>
         </div>
 
-      
+        {/* النص العربي */}
+        <p className="text-base sm:text-lg text-primary font-medium">
+          شركة أساد النموذجية للصيانة والنظافة
+        </p>
+
+        {/* النص الإنجليزي */}
+        <p className="text-sm sm:text-base text-gray-400">
+          Asad Model Maintenance and Cleaning Company
+        </p>
       </div>
     </div>
   );
